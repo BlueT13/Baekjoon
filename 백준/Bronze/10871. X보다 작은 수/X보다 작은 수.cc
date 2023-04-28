eb0e83ecbfceac3b2bfb8fa00372b6ab9d255d29
Tae Hwan Lee <87050879+BlueT13@users.noreply.github.com>
2023-04-28 22:46:23 +0900

    [Bronze V] Title: X보다 작은 수, Time: 412 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n, x, input;
+    cin >> n >> x;
+    
+    for(int i = 0; i < n; i++)
+    {
+        cin >> input;
+        if(input < x)
+        {
+            cout << input << " ";
+        }
+    }
+}
