e5ed4fb73e956efabd81a4e07dc0c4e75d6524cb
Tae Hwan Lee <87050879+BlueT13@users.noreply.github.com>
2023-06-01 22:48:45 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -1,60 +1,28 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-
+#include <vector>
 using namespace std;
 
 int main(void)
 {
+	vector<string> croatian = { "c=","c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+	int idx;
 	string input;
 	cin >> input;
 
-	int count = 0;
-
-	for (int i = 0; i < input.length(); i++)
+	for (int i = 0; i < croatian.size(); i++)
 	{
-		if (input[i] == 'c' && (input[i + 1] == '=' || input[i + 1] == '-'))
+		while (true)
 		{
-			count++;
-			i++;
-		}
-		else if (input[i] == 'd')
-		{
-			if (input[i + 1] == '-')
+			idx = input.find(croatian[i]);
+			if (idx == string::npos)
 			{
-				count++;
-				i++;
+				break;
 			}
-			else if (input[i + 1] == 'z' && input[i + 2] == '=')
-			{
-				count++;
-				i += 2;
-			}
-			else
-			{
-				count++;
-			}
-		}
-		else if ((input[i] == 'l' || input[i] == 'n') && input[i + 1] == 'j')
-		{
-			count++;
-			i++;
-		}
-		else if (input[i] == 's' && input[i + 1] == '=')
-		{
-			count++;
-			i++;
-		}
-		else if (input[i] == 'z' && input[i + 1] == '=')
-		{
-			count++;
-			i++;
-		}
-		else
-		{
-			count++;
+			input.replace(idx, croatian[i].size(), "#");
 		}
 	}
 
-	cout << count;
+	cout << input.size();
 }
