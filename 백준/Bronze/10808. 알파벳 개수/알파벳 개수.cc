4ff43d29fdc1cc4be2661e329b66b842dc0fbee1
Tae Hwan Lee <87050879+BlueT13@users.noreply.github.com>
2024-06-13 16:52:12 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -7,27 +7,27 @@ int main()
 	std::cin >> S;
 
 	//Vector
-	//std::vector<int> Counts(26);
-	//for (int i = 0; i < S.size(); i++)
-	//{
-	//	Counts[S[i] - 'a']++;
-	//}
-
-	//for (int i = 0; i < Counts.size(); i++)
-	//{
-	//	std::cout << Counts[i] << " ";
-	//}
-
-	// Array
-	int Counts[26] = {};
+	std::vector<int> Counts(26);
 	for (int i = 0; i < S.size(); i++)
 	{
 		Counts[S[i] - 'a']++;
 	}
 
-	for (int i = 0; i < sizeof(Counts) / sizeof(int); i++)
+	for (int i = 0; i < Counts.size(); i++)
 	{
 		std::cout << Counts[i] << " ";
 	}
+
+	// Array
+	//int Counts[26] = {};
+	//for (int i = 0; i < S.size(); i++)
+	//{
+	//	Counts[S[i] - 'a']++;
+	//}
+
+	//for (int i = 0; i < sizeof(Counts) / sizeof(int); i++)
+	//{
+	//	std::cout << Counts[i] << " ";
+	//}
 
 }
