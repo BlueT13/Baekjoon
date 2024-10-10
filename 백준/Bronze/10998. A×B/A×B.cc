0069e4f11f2df2ee32314bf83287decd4b326dd4
Tae Hwan Lee <87050879+BlueT13@users.noreply.github.com>
2024-10-10 22:01:11 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.cc
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -1,12 +1,8 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
-    int A, B;
-    
-    cin >> A >> B;
-    cout << A * B;
-    
-    return 0;
+    int A,B;
+    std::cin >> A >> B;
+    std::cout << A*B;
 }
